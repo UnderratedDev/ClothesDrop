08b677d491cf49d1f75f2825a856d015812be580
honeychama <joannehs0@hotmail.com>
2017-05-10 10:06:59 -0700

    Added buttons to the activity_request_details.xml

--- app/src/main/java/nestedternary/project/MainSchedulingActivity.java
+++ app/src/main/java/nestedternary/project/MainSchedulingActivity.java
@@ -1,7 +1,9 @@
 package nestedternary.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainSchedulingActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainSchedulingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_scheduling);
+    }
+
+    public void schedulingDetails (final View view) {
+        Intent intent = new Intent (MainSchedulingActivity.this, RequestDetailsActivity.class);
+        startActivity (intent);
+
     }
 }
