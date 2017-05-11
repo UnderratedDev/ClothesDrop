ba70efac90edaa98be81215bc77ceda786177304
honeychama <joannehs0@hotmail.com>
2017-05-11 11:32:30 -0700

    Populate spinner with regions from the server

--- app/src/main/java/nestedternary/project/MainSchedulingActivity.java
+++ app/src/main/java/nestedternary/project/MainSchedulingActivity.java
@@ -3,7 +3,18 @@ package nestedternary.project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainSchedulingActivity extends AppCompatActivity {
 
@@ -13,9 +24,66 @@ public class MainSchedulingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_scheduling);
     }
 
-    public void schedulingDetails (final View view) {
+    public void schedulingDetails (final View view, ArrayList<String> regionInfo) {
+
+
         Intent intent = new Intent (MainSchedulingActivity.this, RequestDetailsActivity.class);
-        startActivity (intent);
+
+        if(regionInfo.size() == 0)
+        {
+            Toast.makeText(MainSchedulingActivity.this,
+                    "Error with connection please try again",
+                    Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            intent.putStringArrayListExtra("regionList", regionInfo);
+
+            startActivity(intent);
+        }
+
+
+    }
+
+    public void jsonRequest(final View view)
+    {
+        final ArrayList<String> ListRegions = new ArrayList<String>();
+        Ion.with(this).
+                load("http://mail.posabilities.ca:8000/api/getregions.php").
+                asJsonArray().
+                setCallback(
+                        new FutureCallback<JsonArray>()
+                        {
+
+                            @Override
+                            public void onCompleted(final Exception ex,
+                                                    final JsonArray array)
+                            {
+                                if(ex != null)
+                                {
+                                    Toast.makeText(MainSchedulingActivity.this,
+                                            "Error: " + ex.getMessage(),
+                                            Toast.LENGTH_LONG).show();
+
+                                }
+                                else
+                                {
+                                    for(final JsonElement element : array)
+                                    {
+
+                                        final JsonObject json;
+                                        final JsonElement nameElement;
+                                        final String      name;
+
+                                        json              = element.getAsJsonObject();
+                                        nameElement       = json.get("regionname");
+                                        name              = nameElement.getAsString();
+                                        ListRegions.add(name);
+                                    }
+                                }
+                                schedulingDetails(view, ListRegions);
+                            }
+                        });
 
     }
 }
--- app/src/main/java/nestedternary/project/RequestDetailsActivity.java
+++ app/src/main/java/nestedternary/project/RequestDetailsActivity.java
@@ -1,7 +1,21 @@
 package nestedternary.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.util.ArrayList;
 
 public class RequestDetailsActivity extends AppCompatActivity {
 
@@ -9,5 +23,21 @@ public class RequestDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_details);
+        Intent intent = getIntent ();
+        ArrayList<String> regionList = intent.getStringArrayListExtra("regionList");
+
+
+        Spinner regions = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, regionList);
+
+
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Log.e ("MEOW", "" + regionList.size ());
+
+        regions.setAdapter(adapter);
     }
+
+
 }
