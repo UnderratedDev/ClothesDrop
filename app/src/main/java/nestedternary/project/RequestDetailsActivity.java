120dfff896bbb8cec30ec32f1fd35ba2427c6a60
honeychama <joannehs0@hotmail.com>
2017-05-11 13:52:21 -0700

    Automatically fills address if GPS is enabled, and added cancel button functionality

--- app/src/main/java/nestedternary/project/RequestDetailsActivity.java
+++ app/src/main/java/nestedternary/project/RequestDetailsActivity.java
@@ -1,12 +1,21 @@
 package nestedternary.project;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.JsonArray;
@@ -16,6 +25,7 @@ import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RequestDetailsActivity extends AppCompatActivity {
 
@@ -25,7 +35,7 @@ public class RequestDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_request_details);
         Intent intent = getIntent ();
         ArrayList<String> regionList = intent.getStringArrayListExtra("regionList");
-
+        String address = intent.getStringExtra("location");
 
         Spinner regions = (Spinner)findViewById(R.id.spinner);
         ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, regionList);
@@ -33,10 +43,18 @@ public class RequestDetailsActivity extends AppCompatActivity {
 
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        Log.e ("MEOW", "" + regionList.size ());
-
+        TextView location = (TextView)findViewById(R.id.location);
         regions.setAdapter(adapter);
+        if(location != null)
+            location.setText(address);
+
+
+
+
+    }
+
+    public void cancel(View view){
+        finish();
     }
 
 
