fab3e2efaef32463a272f8aaffb907fcc2fabade
David <daaaveyu@gmail.com>
2017-05-17 18:48:08 -0700

    added activities for viewing scheduled requests and editing scheduled requests

---
+++ app/src/main/java/nestedternary/project/ScheduledRequestDetailsActivity.java
@@ -0,0 +1,13 @@
+package nestedternary.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScheduledRequestDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scheduled_request_details);
+    }
+}
