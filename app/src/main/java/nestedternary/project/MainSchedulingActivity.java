1619b60d90858653e61d175ad7c1a587b8f37656
Yudhvir Raj <rajyudhvir29@gmail.com>
2017-05-22 23:15:54 -0700

    Bug Fix, (old bug) double listing of pickups
    David alerted me to this issue.
    Looking at the lifecycle methods, oncreate and onresume reside on the same line therefore double listing.
    Removed from onCreate

--- app/src/main/java/nestedternary/project/MainSchedulingActivity.java
+++ app/src/main/java/nestedternary/project/MainSchedulingActivity.java
@@ -52,8 +52,6 @@ public class MainSchedulingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_scheduling);
-
-        recieveData ();
     }
 
     public String URL() {
